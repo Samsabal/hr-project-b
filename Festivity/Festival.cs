4f4dad8de1d24bc2e870b0049bfd540cb2b71b4f
Kelvin120 <57939956+Kelvin120@users.noreply.github.com>
2020-03-30 17:49:58 +0200

    Setup database 3.0

---
+++ Festivity/Festival.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Festivity
+{
+    class Festival
+    {
+        [JsonProperty("Id")]
+        public int Id { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("Location")]
+        public string Location { get; set; }
+        [JsonProperty("Date")]
+        public string Date { get; set; }
+        [JsonProperty("Time")]
+        public string Time { get; set; }
+    }
+
+    class JSONFestivalList
+    {
+        [JsonProperty("Festivals")]
+        public List<Festival> Festivals { get; set; }
+    }
+}
---
+++ Festivity/FestivalPage.cs
@@ -0,0 +1,32 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Festivity
+{
+    class FestivalPage
+    {
+        static void Main(string[] args)
+        {
+            string PATH_FESTIVAL = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..", @"FestivalsDatabase.json");
+            JSONFestivalList Festivals = JsonConvert.DeserializeObject<JSONFestivalList>(File.ReadAllText(PATH_FESTIVAL));
+
+            void festival()
+            {
+                foreach (var festival in Festivals.Festivals)
+                {
+                    Console.WriteLine(festival.Id);
+
+                }
+            }
+        }
+
+    }
+
+}
