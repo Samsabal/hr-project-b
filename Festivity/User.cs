d4c5006f55a2113e530876fde524ae52f79e0e7c
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-04-05 18:32:13 +0200

    Added classes
    Added UserAdmin, UserOrganisator and UserVisitory class

--- Festivity/User.cs
+++ Festivity/User.cs
@@ -5,14 +5,42 @@ namespace Festivity
 {
     class User
     {
-        [JsonProperty("Id")]
+        [JsonProperty("firstName")]
         public int Id { get; set; }
-        [JsonProperty("Username")]
+        [JsonProperty("lastName")]
         public string Username { get; set; }
-        [JsonProperty("Password")]
+        [JsonProperty("email")]
         public string Password { get; set; }
+        [JsonProperty("password")]
+        public string password { get; set; }
+        [JsonProperty("accountType")]
+        public string accountType { get; set; }
+        [JsonProperty("accountID")]
+        public string accountID { get; set; }
     }
-    
+
+    class UserAdmin : User
+    {
+
+    }
+
+    class UserOrganisator : User
+    {
+        [JsonProperty("contactPerson")]
+        public int ContactPerson { get; set; }
+        [JsonProperty("phoneNumber")]
+        public int phoneNumber { get; set; }
+        [JsonProperty("companyName")]
+        public int companyName { get; set; }
+        [JsonProperty("companyAddress")]
+        public int phoneNumber { get; set; }
+    }
+
+    class UserVisitor : User
+    {
+
+    }
+
     class JSONUserList
     {
         [JsonProperty("users")]
