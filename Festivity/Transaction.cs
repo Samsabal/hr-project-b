9c1dd228d746cf5b5ef0bfc35902821d7e3b4e9c
Samuel Spek <samuel.spek@gmail.com>
2020-04-06 15:36:02 +0200

    Added a transaction database.
    Transaction Json database added.  This wil be used to keep track of all transaction.

---
+++ Festivity/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Festivity
+{
+    class Transaction
+    {
+        [JsonProperty("FestivalID")]
+        public int FestivalID { get; set; }
+        [JsonProperty("TicketID")]
+        public int TicketID { get; set; }
+        [JsonProperty("BuyerID")]
+        public int BuyerID { get; set; }
+        [JsonProperty("TicketNumber")]
+        public int TicketNumber { get; set; }
+        [JsonProperty("OrderNumber")]
+        public int OrderNumber { get; set; }
+    }
+
+    class JSONTransactionList
+    {
+        [JsonProperty("transactions")]
+        public List<Transaction> transactions { get; set; }
+    }
+}
