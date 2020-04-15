e8abce5877eb392afd6a7a433938d340b89bbb6b
robinfredriks <57940911+robinfredriks@users.noreply.github.com>
2020-04-15 14:01:45 +0200

    Update CatalogPageFilter.cs

--- Festivity/CatalogPageFilter.cs
+++ Festivity/CatalogPageFilter.cs
@@ -1,57 +1,56 @@
-﻿//namespace Festivity
-//{
-//    class CatalogPageFilter
-//    {
-//        // Receives a Festival array and sorts it in alphabetical order by name
-//        public static Festival[] sort_name(Festival[] festivalArray, int arraySize)
-//        {
-//            for (int j = arraySize - 1; j > 0; j--)
-//            {
-//                for (int i = 0; i < j; i++)
-//                {
-//                    if (festivalArray[i].Name.CompareTo(festivalArray[i + 1].Name) > 0)
-//                    {
-//                        Festival temp = festivalArray[i];
-//                        festivalArray[i] = festivalArray[i + 1];
-//                        festivalArray[i + 1] = temp;
-//                    }
-//                }
-//            }
-//            return festivalArray;
-//        }
+﻿namespace Festivity
+{
+    class CatalogPageFilter
+    {
+        // Receives a Festival array and sorts it in alphabetical order by name
+        public static Festival[] sort_name(Festival[] festivalArray, int arraySize)
+        {
+            for (int j = arraySize - 1; j > 0; j--)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if (festivalArray[i].Name.CompareTo(festivalArray[i + 1].Name) > 0)
+                    {
+                        Festival temp = festivalArray[i];
+                        festivalArray[i] = festivalArray[i + 1];
+                        festivalArray[i + 1] = temp;
+                    }
+                }
+            }
+            return festivalArray;
+        }
 
-//        // Receives a Festival array and sorts it in ascending order by date
-//        public static Festival[] sort_date(Festival[] festivalArray, int arraySize)
-//        {
-//            for (int j = arraySize - 1; j > 0; j--)
-//            {
-//                for (int i = 0; i < j; i++)
-//                {
-//                    if (festivalArray[i].Date.year > festivalArray[i + 1].Date.year)
-//                    {
-//                        Festival temp = festivalArray[i];
-//                        festivalArray[i] = festivalArray[i + 1];
-//                        festivalArray[i + 1] = temp;
-//                    }
-//                    else if (festivalArray[i].Date.year == festivalArray[i + 1].Date.year &&
-//                        festivalArray[i].Date.month > festivalArray[i + 1].Date.month)
-//                    {
-//                        Festival temp = festivalArray[i];
-//                        festivalArray[i] = festivalArray[i + 1];
-//                        festivalArray[i + 1] = temp;
-//                    }
-//                    else if (festivalArray[i].Date.year == festivalArray[i + 1].Date.year &&
-//                        festivalArray[i].Date.month == festivalArray[i + 1].Date.month &&
-//                        festivalArray[i].Date.day > festivalArray[i + 1].Date.day)
-//                    {
-//                        Festival temp = festivalArray[i];
-//                        festivalArray[i] = festivalArray[i + 1];
-//                        festivalArray[i + 1] = temp;
-//                    }
-//                }
-//            }
-//            return festivalArray;
-//        }
-
-//    }
-//}
+        // Receives a Festival array and sorts it in ascending order by date
+        public static Festival[] sort_date(Festival[] festivalArray, int arraySize)
+        {
+            for (int j = arraySize - 1; j > 0; j--)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if (festivalArray[i].Date.year > festivalArray[i + 1].Date.year)
+                    {
+                        Festival temp = festivalArray[i];
+                        festivalArray[i] = festivalArray[i + 1];
+                        festivalArray[i + 1] = temp;
+                    }
+                    else if (festivalArray[i].Date.year == festivalArray[i + 1].Date.year &&
+                        festivalArray[i].Date.month > festivalArray[i + 1].Date.month)
+                    {
+                        Festival temp = festivalArray[i];
+                        festivalArray[i] = festivalArray[i + 1];
+                        festivalArray[i + 1] = temp;
+                    }
+                    else if (festivalArray[i].Date.year == festivalArray[i + 1].Date.year &&
+                        festivalArray[i].Date.month == festivalArray[i + 1].Date.month &&
+                        festivalArray[i].Date.day > festivalArray[i + 1].Date.day)
+                    {
+                        Festival temp = festivalArray[i];
+                        festivalArray[i] = festivalArray[i + 1];
+                        festivalArray[i + 1] = temp;
+                    }
+                }
+            }
+            return festivalArray;
+        }
+    }
+}
