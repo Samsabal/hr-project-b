469cc043d572e25f44cb3f54587f7dfc67cd0316
Enny <theennyk@gmail.com>
2020-06-01 14:38:10 +0200

    Fixed Amount Earned
    - Removed unecessary code

--- Festivity/UserAccountPage.cs
+++ Festivity/UserAccountPage.cs
@@ -27,31 +27,13 @@ namespace Festivity
                 Console.WriteLine($"    {LoggedInAccount.User.userAddress.ZipCode} {LoggedInAccount.User.userAddress.City}");
                 Console.WriteLine($"    {LoggedInAccount.User.Email}");
                 Console.WriteLine();
-                Console.WriteLine($"    Total amount earned: {AmountEarned()} Euro's");
+                if(LoggedInAccount.User.AccountType == 1)
+                {
+                    Console.WriteLine($"    Total amount earned: {AmountEarned()} Euro's");
+                }
                 Console.WriteLine("");
                 MenuFunction.Menu(new string[] { "Change user information", "Change password", "Preference for e-mails", "Exit to Main Menu" });
             }
-            #region Previous Code
-            //foreach (var user in users.Users)
-            //{
-            //    if (LoggedInAccount.GetID() == user.AccountID)
-            //    {
-            //        MenuFunction.option = 0;
-            //        while (true)
-            //        {
-            //            Console.WriteLine();
-            //            Console.WriteLine("Your account Information: ");
-            //            Console.WriteLine();
-            //            Console.WriteLine($"    {user.FirstName} {user.LastName}");
-            //            Console.WriteLine($"    {user.userAddress.StreetName} {user.userAddress.StreetNumber}");
-            //            Console.WriteLine($"    {user.userAddress.ZipCode} {user.userAddress.City}");
-            //            Console.WriteLine($"    {user.Email}");
-            //            Console.WriteLine("");
-            //            MenuFunction.Menu(new string[] { "Change user information", "Change password", "Preference for e-mails", "Exit to Main Menu" });
-            //        }
-            //    }
-            //}
-            #endregion Previous Code
         }
 
         public static void AccountChangeInfo()
@@ -149,50 +131,6 @@ namespace Festivity
                     AccountEmailPrefference();
                 }
             }
-            #region Previous Code
-            //foreach (var user in users.Users)
-            //{
-            //    if (UserLoginPage.currentUserID == user.AccountID)
-            //    {
-            //        if (user.NewsLetter == 1)
-            //        {
-            //            Console.WriteLine("Do you want to stop recieving Newsletters? [Y or N].");
-            //            string userInput = Console.ReadLine();
-            //            if (userInput.ToLower() == "y")
-            //            {
-            //                Console.Clear();
-            //                user.NewsLetter = 0;
-            //                Console.WriteLine("Preference successfully changed.");
-            //                Thread.Sleep(1000);
-            //            }
-            //            else if (userInput.ToLower() != "n")
-            //            {
-            //                Console.Clear();
-            //                Console.WriteLine("Invalid input, please try again.");
-            //                AccountEmailPrefference();
-            //            }
-            //        }
-            //        else if (user.NewsLetter == 0)
-            //        {
-            //            Console.WriteLine("Do you want to recieve Newsletters? [Y or N]");
-            //            string userInput = Console.ReadLine();
-            //            if (userInput.ToLower() == "y")
-            //            {
-            //                Console.Clear();
-            //                user.NewsLetter = 1;
-            //                Console.WriteLine("Preference successfully changed.");
-            //                Thread.Sleep(1000);
-            //            }
-            //            else if (userInput.ToLower() != "n")
-            //            {
-            //                Console.Clear();
-            //                Console.WriteLine("Invalid input, please try again.");
-            //                AccountEmailPrefference();
-            //            }
-            //        }
-            //    }
-            //}
-            #endregion Previous Code
             LoggedInAccount.UpdateDatabase();
         }
 
@@ -341,158 +279,6 @@ namespace Festivity
                         break;
                 }
             }
-            #region Previous Code
-            //foreach (var user in users.Users)
-            //{
-            //    if (UserLoginPage.currentUserID == user.AccountID)
-            //    {
-            //        if (UserLoginPage.currentUserType == 1)
-            //        {
-            //            switch (userSelection)
-            //            {
-            //                case 1:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new firstname: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.FirstName = userInput;
-            //                    break;
-
-            //                case 2:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new lastname: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.LastName = userInput;
-            //                    break;
-
-            //                case 3:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new email: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.Email = userInput;
-            //                    break;
-
-            //                case 4:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new streetname: ");
-            //                    string userInput1 = Console.ReadLine();
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new streetnumber: ");
-            //                    string userInput2 = Console.ReadLine();
-            //                    user.userAddress.StreetName = userInput1;
-            //                    user.userAddress.StreetNumber = userInput2;
-            //                    break;
-
-            //                case 5:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new city: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.City = userInput;
-            //                    break;
-
-            //                case 6:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new zipcode: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.ZipCode = userInput;
-            //                    break;
-
-            //                case 7:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new country: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.Country = userInput;
-            //                    break;
-
-            //                case 8:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new companyname: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.CompanyName = userInput;
-            //                    break;
-
-            //                case 9:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new companynumber: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.CompanyPhoneNumber = userInput;
-            //                    break;
-
-            //                default:
-            //                    break;
-            //            }
-            //        }
-            //        if (UserLoginPage.currentUserType == 2) // Festival Goer
-            //        {
-            //            switch (userSelection)
-            //            {
-            //                case 1:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new firstname: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.FirstName = userInput;
-            //                    break;
-
-            //                case 2:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new lastname: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.LastName = userInput;
-            //                    break;
-
-            //                case 3:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new email: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.Email = userInput;
-            //                    break;
-
-            //                case 4:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new phoneNumber: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.PhoneNumber = userInput;
-            //                    break;
-
-            //                case 5:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new streetname: ");
-            //                    string userInput1 = Console.ReadLine();
-            //                    Console.WriteLine("Input new streetnumber: ");
-            //                    string userInput2 = Console.ReadLine();
-            //                    user.userAddress.StreetName = userInput1;
-            //                    user.userAddress.StreetNumber = userInput2;
-            //                    break;
-
-            //                case 6:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new city: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.City = userInput;
-            //                    break;
-
-            //                case 7:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new zipcode: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.ZipCode = userInput;
-            //                    break;
-
-            //                case 8:
-            //                    Console.Clear();
-            //                    Console.WriteLine("Input new country: ");
-            //                    userInput = Console.ReadLine();
-            //                    user.userAddress.Country = userInput;
-            //                    break;
-
-            //                default:
-            //                    break;
-            //            }
-            //        }
-            //        string json = JsonConvert.SerializeObject(users, Formatting.Indented);
-            //        File.WriteAllText(PathUser, json);
-            //    }
-            //}
-            #endregion Previous Code
             LoggedInAccount.UpdateDatabase();
         }
 
@@ -530,45 +316,6 @@ namespace Festivity
                 Thread.Sleep(1000);
                 ChangePassword();
             }
-
-            #region Previous Code
-            //foreach (var user in users.Users)
-            //{
-            //    if (UserLoginPage.currentUserID == user.AccountID)
-            //    {
-            //        Console.WriteLine("Input current password: ");
-            //        userInput = Console.ReadLine();
-            //        Console.Clear();
-            //        if (userInput == user.Password)
-            //        {
-            //            Console.WriteLine("Input new password: ");
-            //            string userInput1 = Console.ReadLine();
-            //            Console.Clear();
-            //            Console.WriteLine("Input new password again: ");
-            //            string userInput2 = Console.ReadLine();
-            //            Console.Clear();
-            //            if (userInput1 == userInput2)
-            //            {
-            //                user.Password = userInput2;
-            //                Console.WriteLine("Password successfully changed.");
-            //                Thread.Sleep(1000);
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine("Passwords do not match, please try again.");
-            //                ChangePassword();
-            //            }
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Invalid password, please try again.");
-            //            ChangePassword();
-            //        }
-            //    }
-            //}
-            //string json = JsonConvert.SerializeObject(users, Formatting.Indented);
-            //File.WriteAllText(PathUser, json);
-            #endregion Previous Code
             LoggedInAccount.UpdateDatabase();
         }
 
