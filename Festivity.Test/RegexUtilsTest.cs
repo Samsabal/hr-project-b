3d927062a185aecb8433fa2268b08ee0e35fb49c
Enny <theennyk@gmail.com>
2020-06-03 16:10:49 +0200

    Created RegexUtils.IsValidName unit test
    Learning purpose. DO NOT PUSH THIS BRANCH

---
+++ Festivity.Test/RegexUtilsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using Festivity;
+
+namespace Festivity.Test
+{
+    [TestFixture]
+    public class RegexUtilsTest
+    {
+        [Test]
+        public void IsValidName_ShouldValidateNormalNames()
+        {
+            // Arrange
+            bool expected = true;
+
+            // Act
+            bool actual = RegexUtils.IsValidName("12");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
