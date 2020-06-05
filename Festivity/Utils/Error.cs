2c8f96b1b7c0743b92e64955b11bdce523b26441
Samuel Spek <samuel.spek@gmail.com>
2020-06-05 16:33:25 +0200

    Create Error.cs for Error Messages
    You will now be able to call red error messages.

---
+++ Festivity/Utils/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Festivity
+{
+    class Error
+    {
+        public static void Write(string errorMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMessage);
+            Console.ResetColor();
+        }
+    }
+}
