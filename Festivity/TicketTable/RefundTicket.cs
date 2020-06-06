e033f35b8662be610df8c3c55ac13c21952ed610
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-06-06 13:08:09 +0200

    Now you can't refund tickets that are no refundable.

--- Festivity/TicketTable/RefundTicket.cs
+++ Festivity/TicketTable/RefundTicket.cs
@@ -9,6 +9,7 @@ namespace Festivity
     {
         private static string userInput;
         private static List<string> TransactionIDList = new List<string>();
+        private static List<string> TransactionIsRefundable = new List<string>();
 
 
         public static void InitiateRefund()
@@ -54,8 +55,18 @@ namespace Festivity
                 {
                     if (TransactionIDList[i] == result.ToString())
                     {
-                        TransactionIDList.RemoveAt(i);
-                        return true;
+                        if(TransactionIsRefundable[i].ToLower() == "true")
+                        {
+                            TransactionIDList.RemoveAt(i);
+                            return true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ticket is not refundable.");
+                            Thread.Sleep(2000);
+                            return false;
+                        }
+
                     }
                 }
                 Console.WriteLine("Transaction ID does not exist, please try again");
@@ -73,6 +84,7 @@ namespace Festivity
             for (int i = 0; i < ticketList.Count; i ++)
             {
                 TransactionIDList.Add(ticketList[i][7]);
+                TransactionIsRefundable.Add(ticketList[i][3]);
             }
         }
     }
