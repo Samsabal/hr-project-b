570bc21ffe7e94483c994ba6618eb8e95f2b5630
Samuel Spek <samuel.spek@gmail.com>
2020-06-06 14:22:45 +0200

    Adds AgeCheck to Utils.

--- Festivity/Festival/FestivalPage/Utils.cs
+++ Festivity/Festival/FestivalPage/Utils.cs
@@ -20,5 +20,33 @@
             }
             return maxTickets;
         }
+
+        public static bool AgeCheck(int festivalId) //Checks if the user is old enough to use the program
+        {
+            foreach (var festival in JSONFunctionality.GetFestivals().Festivals)
+            {
+                if (festival.FestivalID == festivalId)
+                {
+                    foreach (var user in JSONFunctionality.GetUserList().Users)
+                    {
+                        if (LoggedInAccount.GetID() == user.AccountID)
+                        {
+                            int userAgeYear = festival.FestivalDate.Year - user.BirthDate.Year;
+                            int userAgeMonth = festival.FestivalDate.Month - user.BirthDate.Month;
+                            int userAgeDay = festival.FestivalDate.Day - user.BirthDate.Day;
+                            if (userAgeYear * 365 + userAgeMonth * 30 + userAgeDay > festival.FestivalAgeRestriction * 365)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
