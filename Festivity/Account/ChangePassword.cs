4c527a1092a84d0e31dd53fcdd2e5f25ed490f4e
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-06-07 10:57:53 +0200

    Refractored ChangePassword class
    - Added General class
    - Added Inputloop Method to Utils.General class

--- Festivity/Account/ChangePassword.cs
+++ Festivity/Account/ChangePassword.cs
@@ -7,40 +7,59 @@ namespace Festivity.Account
 {
     class ChangePassword
     {
+        private static string Password;
+
         public static void Initiate()
         {
-            Console.WriteLine("Input current password: ");
-            string userInput = Console.ReadLine();
-
-            if (userInput == LoggedInAccount.User.Password)
+            Console.Write("Input current password: ");
+            if (UserInputIsCorrectPassword())
             {
-                Console.WriteLine("Input new password: ");
-                string userInput1 = Console.ReadLine();
-                ConsoleHelperFunctions.ClearCurrentConsoleLine();
-                Console.WriteLine("Input new password again: ");
-                string userInput2 = Console.ReadLine();
-                Console.Clear();
-                if (userInput1 == userInput2)
-                {
-                    LoggedInAccount.User.Password = userInput2;
-                    Console.WriteLine("Password successfully changed.");
-                    Thread.Sleep(1000);
-                    Console.Clear();
-                }
-                else
-                {
-                    Console.WriteLine("Passwords do not match, please try again.");
-                    Thread.Sleep(1000);
-                    Initiate();
-                }
+                Console.Write("Input new password: ");
+                string FirstPassword = AskUserForPasswordWithValidation();
+                Console.Write("Input new password again: ");
+                string SecondPassword = AskForSameUserPassword(FirstPassword);
+                LoggedInAccount.User.Password = SecondPassword;
+                Console.WriteLine("Password successfully changed");
+                Thread.Sleep(1000);
+                ConsoleHelperFunctions.ClearCurrentConsole();
             }
             else
             {
                 Console.WriteLine("Invalid password, please try again.");
-                Thread.Sleep(1000);
                 Initiate();
             }
             LoggedInAccount.UpdateDatabase();
+        }
+
+        private static bool UserInputIsCorrectPassword()
+        {
+            string userInput = Console.ReadLine();
+            return userInput == LoggedInAccount.User.Password;
+        }
+
+
+        private static string AskUserForPasswordWithValidation()
+        {
+            do { Password = Utils.General.InputLoop (""); } 
+            while (!RegexUtils.IsValidPassword(Password));
+            return Password;
+        }
+
+        private static string AskForSameUserPassword(string p1)
+        {
+            do { Password = Utils.General.InputLoop(""); }
+            while (PasswordsDoNotMatch(p1, Password));
+            return Password;
+        }
+
+        private static bool PasswordsDoNotMatch(string p1, string p2)
+        {
+            if(p1 != p2)
+            {
+                Console.Write("Passwords do not match, please try again:");
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ Festivity/Utils/General.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Festivity.Utils
+{
+    class General
+    {
+        public static string InputLoop(string printString)
+        {
+            string userInput;
+            Console.Write(printString); userInput = Console.ReadLine();
+            Console.Clear();
+            return userInput;
+        }
+    }
+}
