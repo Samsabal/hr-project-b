872925c89c8deb66ac18bae1af97506199b7b78b
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-06-07 22:20:48 +0200

    Added PathString Class

---
+++ Festivity/Utils/PathString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Festivity.Utils
+{
+    class PathString
+    {
+        private string PathOne;
+        private string PathTwo;
+        private string Size;
+
+        //public PathString()
+        //{
+        //    Size = "One";
+            
+        //}
+
+        public PathString(string p1)
+        {
+            PathOne = p1;
+            Size = "Two";
+        }
+
+        public PathString(string p1, string p2)
+        {
+            PathOne = p1;
+            PathTwo = p2;
+            Size = "Three";
+        }
+
+        public void Draw()
+        {
+            DrawDikkeLine();
+            switch (Size)
+            {
+                //case "One":                 
+                //    Console.WriteLine($"                Home                 ");
+                //    break;
+                case "Two":
+                    Console.WriteLine($"          Home > {PathOne}          ");
+                    break;
+                case "Three":
+                    Console.WriteLine($"     Home > {PathOne} > {PathTwo}   ");
+                    break;
+                default:
+                    ErrorMessage.WriteLine("Draw Error");
+                    break;
+            }
+            DrawDikkeLine();
+        }
+
+        private void  DrawDikkeLine()
+        {
+            Console.WriteLine($"                                    ");
+        }
+    }
+}
