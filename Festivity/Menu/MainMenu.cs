91ee27bd1825fab1384fdd5b4d8ae02cd86ebc79
Samuel Spek <samuel.spek@gmail.com>
2020-06-08 21:48:49 +0200

    Merge fix from Dev

--- Festivity/Menu/MainMenu.cs
+++ Festivity/Menu/MainMenu.cs
@@ -8,29 +8,30 @@ namespace Festivity
 {
     class MainMenu : MenuBuilder
     {
+        private static UIElements UI = new UIElements();
         public static List<MenuOption> MainMenuBuilder()
         {
             // Create List of menu options
             List<MenuOption> newMenuOptions = new List<MenuOption>
             {
-                new MenuOption("Register", () =>
+                new MenuOption(UI.SpaceStringInMiddle(". Register ."), () =>
                 {
                     Console.Clear();
                     Menu.OptionReset();
                     UserModel user = new UserModel(){AccountID = JSONFunctionality.GenerateUserID()};
                     UserRegisterHandler.InitiateUserRegister(user);
                 }),
-                new MenuOption("Login", () =>
+                new MenuOption(UI.SpaceStringInMiddle(". Log In ."), () =>
                 {
                     Console.Clear();
                     LoginManager.InitiateLogin();
                 }),
-                new MenuOption("Festivals", () =>
+                new MenuOption(UI.SpaceStringInMiddle(". Festivals ."), () =>
                 {
                     Console.Clear();
                     CatalogPage.CatalogMain();
                 }),
-                new MenuOption("Exit", () =>
+                new MenuOption(UI.SpaceStringInMiddle(". Exit ."), () =>
                 {
                     Environment.Exit(0);
                 })
