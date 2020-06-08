699d78103147bf948112bc9dfdfcfae312805eb5
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-06-08 19:42:01 +0200

    Reworked UserRegister
    - Deleted AccountRegistration\Utils.cs
    - Deleted Registration.cs
    - Deleted AccountRegistration\Manager.cs
    - Added UserModifier.cs
    - Added UserRegisterMenu.cs
    - Added UserRegisterHandler.cs

---
+++ Festivity/AccountRegistration/UserRegisterHandler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Festivity.Utils;
+
+namespace Festivity
+{
+    class UserRegisterHandler
+    {
+        private static readonly JSONUserList userList = JSONFunctionality.GetUserList();
+
+        public static void InitiateUserRegister(UserModel user)
+        {
+            UserModifier.InputFirstName(user);
+            UserModifier.InputLastName(user);
+            UserModifier.InputEmail(user);
+            UserModifier.InputPassword(user);
+            UserModifier.InputAccountType(user);
+
+            if (user.AccountType == 1)
+            {
+                UserModifier.InputCompanyContactperson(user);
+                UserModifier.InputCompanyPhonenumber(user);
+                UserModifier.InputCompanyName(user);
+                UserModifier.InputIBAN(user);
+            }
+
+            if (user.AccountType == 2)
+            {
+                UserModifier.InputBirthday(user);
+                UserModifier.InputVisitorPhonenumber(user);
+            }
+
+            UserModifier.InputUserAdress(user);
+            ShowUserRegister(user);
+        }
+
+        public static void ShowUserRegister(UserModel user)
+        {
+            while (true)
+            {
+                if (user.AccountType == 1)
+                {
+                    Menu.Draw(UserRegisterMenu.UserOrganisatorRegisterMenuBuilder(user));
+                }
+
+                if (user.AccountType == 2)
+                {
+                    Menu.Draw(UserRegisterMenu.UserVisitorRegisterMenuBuilder(user));
+                }
+
+
+            }
+        }
+
+        public static bool CheckIfEmailExists(string email)
+        {
+            bool exists = false;
+            JSONUserList userList = JSONFunctionality.GetUserList();
+
+            foreach (var user in userList.Users)
+            {
+                if (user.Email == email)
+                {
+                    ErrorMessage.WriteLine("Email already in use, please try again");
+                    exists = true;
+                }
+            }
+            return exists;
+        }
+
+        public static bool YesOrNoAccountType()
+        {
+            ConsoleKey input;
+            Console.WriteLine("Are you a Visitor? [y/n]");
+            do { input = General.InputLoopKey(); }
+            while (General.YesOrNoCheck(input));
+            return input == ConsoleKey.Y;
+        }
+
+        public static bool YesOrNoNewsLetter()
+        {
+            ConsoleKey input;
+            Console.WriteLine("Do you want to recieve our newsletter? [y/n]");
+            do { input = General.InputLoopKey(); }
+            while (General.YesOrNoCheck(input));
+
+            Console.Clear();
+            return input == ConsoleKey.Y;
+
+        }
+
+        public static bool IsInputYes(string input)
+        {
+            return RegexUtils.EqualsYesRegex(input);
+        }
+    }
+}
--- Festivity/Utils/General.cs
+++ Festivity/Utils/General.cs
@@ -10,7 +10,34 @@ namespace Festivity.Utils
         {
             string userInput;
             Console.Write(printString); userInput = Console.ReadLine();
-            Console.Clear();
+            return userInput;
+        }
+
+        public static bool YesOrNoCheck(ConsoleKey input)
+        {
+            if (input != ConsoleKey.Y && input != ConsoleKey.N)
+            {
+                ErrorMessage.WriteLine("Wrong input, please press 'y' or 'n'");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static string InputLoopString()
+        {
+            string userInput;
+            userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        public static ConsoleKey InputLoopKey()
+        {
+            ConsoleKey userInput;
+            userInput = Console.ReadKey(true).Key;
             return userInput;
         }
     }
