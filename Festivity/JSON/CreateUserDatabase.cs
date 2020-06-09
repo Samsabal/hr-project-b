81e5f1aa47863d9a1be9e46dfc306fd4296b41b0
Samuel Spek <samuel.spek@gmail.com>
2020-06-09 16:16:00 +0200

    JSON Check if json files doesn't exist

---
+++ Festivity/JSON/CreateUserDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Festivity.JSON
+{
+    class CreateUserDatabase
+    {
+        public static string Check()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Database", @"UserDatabase.json");
+            if (!File.Exists(path))
+            {
+                using StreamWriter file = File.CreateText(path);
+                file.WriteLine("{");
+                file.WriteLine("  \"users\": []");
+                file.WriteLine("}");
+            }
+            return path;
+        }
+    }
+}
